38ebb013321c12ae70197c1e722e7e4ce344b865
Lars Corbijn <aspergesoepje@gmail.com>
2013-08-28 21:24:47 +0200

    Adds parsing the registry.

--- src/Text/OpenGL.hs
+++ src/Text/OpenGL.hs
@@ -1,7 +1,9 @@
 module Text.OpenGL(
     module Text.OpenGL.Types,
-    module Text.OpenGL.Xml.PreProcess
+    module Text.OpenGL.Xml.PreProcess,
+    module Text.OpenGL.Xml.ReadRegistry
 ) where
 
 import Text.OpenGL.Types
 import Text.OpenGL.Xml.PreProcess
+import Text.OpenGL.Xml.ReadRegistry
